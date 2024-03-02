8d1080c4681170190f6dd0dfe868df4e9ab68030
Eddy Jo <eddyjo@Eddys-MacBook-Pro.local>
2024-03-02 00:06:21 -0500

    feat: add reactive form using angular 17 new control flow

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  get emailControl() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password');
+  }  
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+  }
+}
